612bbb78cd375d32b33b7716336f5752189963ee
Erik Jensen <ejensen@netradius.com>
2021-11-03 10:17:49 -0600

    Fixing path issue

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ data "aws_iam_policy_document" "task_role_policy" {
         "ssm:GetParametersByPath"
       ]
       resources = [
-        "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter${statement.value}"
+        "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${statement.value}"
       ]
     }
   }
