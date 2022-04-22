e10658ce83e68dcbfe34ea04feec6b4eeea2bad9
Erik Jensen <ejensen@truemark.io>
2022-04-22 08:09:54 -0600

    Adding outputs for log groups

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,19 @@ output "task_role_arn" {
 output "task_role_name" {
   value = aws_iam_role.task.name
 }
+
+output "cloudwatch_log_group_id" {
+  value = aws_cloudwatch_log_group.service.id
+}
+
+output "cloudwatch_log_group_arn" {
+  value = aws_cloudwatch_log_group.service.arn
+}
+
+output "cloudwatch_log_group_name" {
+  value = aws_cloudwatch_log_group.service.name
+}
+
+output "cloudwatch_log_group_name_prefix" {
+  value = aws_cloudwatch_log_group.service.name_prefix
+}
